6aae34559991b996d946b655c59dabef8037877d
unknown <carlosian894@gmail.com>
2022-05-21 16:11:50 -0300

    relogio

---
+++ script.js
@@ -0,0 +1,16 @@
+function atualizarHora(){
+    var hoje = new Date();
+    var hora = hoje.getHours();
+    var minutos = hoje.getMinutes();
+    var segundos = hoje.getSeconds();
+    minutos = checkTime(minutos);
+    segundos = checkTime(segundos);
+    document.getElementById('relogio').innerHTML = hora + ":" + minutos + ":" + segundos;
+    var tempo = setTimeout(atualizarHora, 1000);
+}
+function checkTime(i){
+    if (i < 10){
+        i = "0" + i;
+    }
+    return i;
+}
